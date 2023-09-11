646a248593f27b83afb20421bc4f2841399a2e9b
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-09-11 15:58:57 -0600

    Update shuntingyard.py

--- shuntingyard.py
+++ shuntingyard.py
@@ -1,32 +1,38 @@
-def shunting_yard(expresion_infija):
-  
-  precedencia = {'+':1, '*':2}
-  salida = []
-  pila_operadores = []
-
-  for token in expresion_infija:
-
-    if token.isalnum(): # operandos
-      salida.append(token)
+def shunting_yard(expresion):
+    precedencia = {'+': 1, '*': 2}
+    salida = []
+    pila_operadores = []
+    utlimoToken = []
     
-    elif token == '(':
-      pila_operadores.append(token)
-
-    elif token == ')':
-      while pila_operadores[-1] != '(':
-        salida.append(pila_operadores.pop())
-      pila_operadores.pop() # quitar '('
+    for token in expresion:
+        if token.isalnum() or token == 'ε':
+            if utlimoToken and utlimoToken[-1].isalnum():
+                salida.append(token)
+                salida.append('.')
+            else:
+                salida.append(token)
+        elif token in precedencia:
+            while pila_operadores and pila_operadores[-1] in precedencia and precedencia[token] <= precedencia[pila_operadores[-1]]:
+                salida.append(pila_operadores.pop())
+            pila_operadores.append(token)
+        elif token == '(':
+            pila_operadores.append(token)
+        elif token == ')':
+            while pila_operadores and pila_operadores[-1] != '(':
+                salida.append(pila_operadores.pop())
+            if pila_operadores and pila_operadores[-1] == '(' and not len(pila_operadores) > 1 :
+                salida.append(".")
+                pila_operadores.pop()
+            elif pila_operadores:
+                pila_operadores.pop()
+        utlimoToken.append(token)
     
-    elif token in precedencia:
-      while pila_operadores and precedencia[pila_operadores[-1]] >= precedencia[token]:
+    while pila_operadores:
         salida.append(pila_operadores.pop())
-      pila_operadores.append(token)
+    
+    return ''.join(salida)
 
-  # agregar operadores restantes
-  while pila_operadores: 
-    salida.append(pila_operadores.pop())
-
-  return ''.join(salida)
-
-expresion = input("Expresión infija: ")
-print("Expresión postfija:", shunting_yard(expresion))
+# Ejemplo de uso
+expresion_infija = input("Expresión en notación infija: ")
+expresion_postfija = shunting_yard(expresion_infija)
+print("Expresión en notación postfija:", expresion_postfija)
