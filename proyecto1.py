2f19bd2c05937c825259328d68e68858db1d1cab
Javier Alejandro Ramírez Heredia <78290724+XJ4v1erX@users.noreply.github.com>
2023-09-26 23:13:02 -0600

    Update proyecto1.py

--- proyecto1.py
+++ proyecto1.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import time
 from graphviz import Digraph
 
 abParentesis, cerrParentesis = '(', ')'
@@ -56,6 +57,40 @@ class Automata:
                 actualizar.crearTransicion(translations[desdeEstado], translations[estado], haciaEstados[estado])
         return [actualizar, startnum]
 
+    def actualizarEstadosIguales(self, equivalent, pos):
+        actualizar = Automata(self.simbolo)
+        for desdeEstado, haciaEstados in self.transiciones.items():
+            for estado in haciaEstados:
+                actualizar.crearTransicion(pos[desdeEstado], pos[estado], haciaEstados[estado])
+        actualizar.iniciarAutomata(pos[self.estadoInicial])
+        for s in self.estadosAceptados:
+            actualizar.aceptarEstado(pos[s])
+        return actualizar
+
+    def cerraduraEpsilon(self, findstate):
+        allEstados = set()
+        estados = [findstate]
+        while len(estados):
+            estado = estados.pop()
+            allEstados.add(estado)
+            if estado in self.transiciones:
+                for tos in self.transiciones[estado]:
+                    if epsilon in self.transiciones[estado][tos] and \
+                        tos not in allEstados:
+                        estados.append(tos)
+        return allEstados
+
+    def getMovimiento(self, estado, skey):
+        if isinstance(estado, int):
+            estado = [estado]
+        trstates = set()
+        for st in estado:
+            if st in self.transiciones:
+                for tns in self.transiciones[st]:
+                    if skey in self.transiciones[st][tns]:
+                        trstates.add(tns)
+        return trstates
+
     def display(self, fname, pname):
         fa = Digraph(pname, filename = fname, format = 'png')
         fa.attr(rankdir='LR')
@@ -96,23 +131,23 @@ class RegexToNFA:
 
     @staticmethod
     def handleSimbolo(inputch):   
-        state1 = 1
-        state2 = 2
+        primerEstado = 1
+        siguienteEstado = 2
         basic = Automata(set([inputch]))
-        basic.iniciarAutomata(state1)
-        basic.aceptarEstado(state2)
-        basic.crearTransicion(state1, state2, inputch)
+        basic.iniciarAutomata(primerEstado)
+        basic.aceptarEstado(siguienteEstado)
+        basic.crearTransicion(primerEstado, siguienteEstado, inputch)
         return basic
 
     @staticmethod
     def handleUnion(a, b):   
         [a, m1] = a.actualizarEstados(2)
         [b, m2] = b.actualizarEstados(m1)
-        state1 = 1
-        state2 = m2
+        primerEstado = 1
+        siguienteEstado = m2
         unionFA = Automata(a.simbolo.union(b.simbolo))
-        unionFA.iniciarAutomata(state1)
-        unionFA.aceptarEstado(state2)
+        unionFA.iniciarAutomata(primerEstado)
+        unionFA.aceptarEstado(siguienteEstado)
         unionFA.crearTransicion(unionFA.estadoInicial, a.estadoInicial, epsilon)
         unionFA.crearTransicion(unionFA.estadoInicial, b.estadoInicial, epsilon)
         unionFA.crearTransicion(a.estadosAceptados[0], unionFA.estadosAceptados[0], epsilon)
@@ -125,11 +160,11 @@ class RegexToNFA:
     def handleConcat(a, b):    
         [a, m1] = a.actualizarEstados(1)
         [b, m2] = b.actualizarEstados(m1)
-        state1 = 1
-        state2 = m2 - 1
+        primerEstado = 1
+        siguienteEstado = m2 - 1
         concatFA = Automata(a.simbolo.union(b.simbolo))
-        concatFA.iniciarAutomata(state1)
-        concatFA.aceptarEstado(state2)
+        concatFA.iniciarAutomata(primerEstado)
+        concatFA.aceptarEstado(siguienteEstado)
         concatFA.crearTransicion(a.estadosAceptados[0], b.estadoInicial, epsilon)
         concatFA.guardarTransiciones(a.transiciones)
         concatFA.guardarTransiciones(b.transiciones)
@@ -138,11 +173,11 @@ class RegexToNFA:
     @staticmethod
     def handleKleene(a):  
         [a, m1] = a.actualizarEstados(2)
-        state1 = 1
-        state2 = m1
+        primerEstado = 1
+        siguienteEstado = m1
         kleeneFA = Automata(a.simbolo)
-        kleeneFA.iniciarAutomata(state1)
-        kleeneFA.aceptarEstado(state2)
+        kleeneFA.iniciarAutomata(primerEstado)
+        kleeneFA.aceptarEstado(siguienteEstado)
         kleeneFA.crearTransicion(kleeneFA.estadoInicial, a.estadoInicial, epsilon)
         kleeneFA.crearTransicion(kleeneFA.estadoInicial, kleeneFA.estadosAceptados[0], epsilon)
         kleeneFA.crearTransicion(a.estadosAceptados[0], kleeneFA.estadosAceptados[0], epsilon)
@@ -208,8 +243,182 @@ class RegexToNFA:
         self.nfa.simbolo = simbolo
 
     def mostrarNFA(self):
-        self.nfa.display('nfa.gv', 'automata_finito_no_determinista')
+        self.nfa.display('nfa.gv', 'nondeterministic_finite_state_machine')
 
+class NFAToDFA:
+
+    def __init__(self, nfa):
+        self.construirDFA(nfa)
+
+    def mostrarDFA(self):
+        self.dfa.display('dfa.gv', 'deterministic_finite_state_machine')
+
+    def mostrarMinDFA(self):
+        self.minDFA.display('mindfa.gv', 'min_deterministic_finite_state_machine')
+
+    def construirDFA(self, nfa):    
+        allEstados = dict()  
+        ecerradura = dict()   
+        primerEstado = nfa.cerraduraEpsilon(nfa.estadoInicial)
+        ecerradura[nfa.estadoInicial] = primerEstado
+        cnt = 1 
+        dfa = Automata(nfa.simbolo)
+        dfa.iniciarAutomata(cnt)
+        estados = [[primerEstado, dfa.estadoInicial]] 
+        allEstados[cnt] = primerEstado
+        cnt += 1
+        while len(estados):
+            [state, desdeIndex] = estados.pop()
+            for ch in dfa.simbolo:
+                trstates = nfa.getMovimiento(state, ch)
+                for s in list(trstates):    
+                    if s not in ecerradura:
+                        ecerradura[s] = nfa.cerraduraEpsilon(s)
+                    trstates = trstates.union(ecerradura[s])
+                if len(trstates):
+                    if trstates not in allEstados.values():
+                        estados.append([trstates, cnt])
+                        allEstados[cnt] = trstates
+                        haciaIndex = cnt
+                        cnt += 1
+                    else:
+                        haciaIndex = [k for k, v in allEstados.items() if v  ==  trstates][0]
+                    dfa.crearTransicion(desdeIndex, haciaIndex, ch)
+            for value, state in allEstados.items():
+                if nfa.estadosAceptados[0] in state:
+                    dfa.aceptarEstado(value)
+            self.dfa = dfa
+
+    @staticmethod
+    def estadoRepetido(estados, pos):  
+        cnt = 1
+        change = dict()
+        for st in estados:
+            if pos[st] not in change:
+                change[pos[st]] = cnt
+                cnt += 1
+            pos[st] = change[pos[st]]
+
+    def minimizar(self): 
+        estados = list(self.dfa.estados)
+        haciaestado = dict(set()) 
+
+        
+        for st in estados:
+            for sy in self.dfa.simbolo:
+                if st in haciaestado:
+                    if sy in haciaestado[st]:
+                        haciaestado[st][sy] = haciaestado[st][sy].union(self.dfa.getMovimiento(st, sy))
+                    else:
+                        haciaestado[st][sy] = self.dfa.getMovimiento(st, sy)
+                else:
+                    haciaestado[st] = {sy : self.dfa.getMovimiento(st, sy)}
+                if len(haciaestado[st][sy]):
+                    haciaestado[st][sy] = haciaestado[st][sy].pop()
+                else:
+                    haciaestado[st][sy] = 0
+
+        equal = dict()  
+        pos = dict()    
+
+        
+        equal = {1: set(), 2: set()}
+        for st in estados:
+            if st not in self.dfa.estadosAceptados:
+                equal[1] = equal[1].union(set([st]))
+                pos[st] = 1
+        for fst in self.dfa.estadosAceptados:
+            equal[2] = equal[2].union(set([fst]))
+            pos[fst] = 2
+
+        sinCheck = []
+        cnt = 3 
+        sinCheck.extend([[equal[1], 1], [equal[2], 2]])
+        while len(sinCheck):
+            [equalst, id] = sinCheck.pop()
+            for sy in self.dfa.simbolo:
+                diff = dict()
+                for st in equalst:
+                    if haciaestado[st][sy] == 0:
+                        if 0 in diff:
+                            diff[0].add(st)
+                        else:
+                            diff[0] = set([st])
+                    else:
+                        if pos[haciaestado[st][sy]] in diff:
+                            diff[pos[haciaestado[st][sy]]].add(st)
+                        else:
+                            diff[pos[haciaestado[st][sy]]] = set([st])
+                if len(diff) > 1:
+                    for k, v in diff.items():
+                        if k:
+                            for i in v:
+                                equal[id].remove(i)
+                                if cnt in equal:
+                                    equal[cnt] = equal[cnt].union(set([i]))
+                                else:
+                                    equal[cnt] = set([i])
+                            if len(equal[id]) == 0:
+                                equal.pop(id)
+                            for i in v:
+                                pos[i] = cnt
+                            sinCheck.append([equal[cnt], cnt])
+                            cnt += 1
+                    break
+        if len(equal) == len(estados):
+            self.minDFA = self.dfa
+        else:
+            NFAToDFA.estadoRepetido(estados, pos)
+            self.minDFA = self.dfa.actualizarEstadosIguales(equal, pos)
+
+    def Analysis(self, string):
+        start_time = time.time()
+        string = string.replace('@', epsilon)
+        curst = self.dfa.estadoInicial
+        for ch in string:
+            if ch == epsilon:
+                continue
+            st = list(self.dfa.getMovimiento(curst, ch))
+            if len(st) == 0:
+                print(f"Cadena '{string}' no aceptada. Se traba en el estado {curst} con la entrada '{ch}'")
+                elapsed_time = time.time() - start_time
+                print(f"Tiempo transcurrido: {elapsed_time} segundos")
+                return False
+            next_state = st[0]
+            print(f"Estado actual: {curst}, Simbolo de entrada: {ch}, Siguiente estado: {next_state}")
+            curst = next_state
+        if curst in self.dfa.estadosAceptados:
+            print(f"Cadena '{string}' aceptada.")
+        else:
+            print(f"Cadena '{string}' no aceptada. Se detiene en el estado {curst}.")
+        elapsed_time = time.time() - start_time
+        print(f"Tiempo transcurrido de simulacion: {elapsed_time} segundos")
+        return curst in self.dfa.estadosAceptados
+
+
+    def AnalysisMinimizedDFA(self, string):
+        start_time = time.time()
+        string = string.replace('@', epsilon)
+        curst = self.minDFA.estadoInicial
+        for ch in string:
+            if ch == epsilon:
+                continue
+            st = list(self.minDFA.getMovimiento(curst, ch))
+            if len(st) == 0:
+                print(f"Cadena '{string}' no aceptada. Se traba en el estado {curst} con la entrada '{ch}'")
+                elapsed_time = time.time() - start_time
+                print(f"Tiempo transcurrido: {elapsed_time} segundos")
+                return False
+            next_state = st[0]
+            print(f"Estado actual: {curst}, Simbolo de entrada: {ch}, Siguiente estado: {next_state}")
+            curst = next_state
+        if curst in self.minDFA.estadosAceptados:
+            print(f"Cadena '{string}' aceptada.")
+        else:
+            print(f"Cadena '{string}' no aceptada. Se detiene en el estado {curst}.")
+        elapsed_time = time.time() - start_time
+        print(f"Tiempo transcurrido de simulacion: {elapsed_time} segundos")
+        return curst in self.minDFA.estadosAceptados
 
 def write_to_file(file_name, data):
     with open(file_name, 'w', encoding='utf-8') as f:
@@ -219,12 +428,49 @@ if __name__ == '__main__':
     regex = input('Ingrese la regex: ')
     a = RegexToNFA(regex)
     a.mostrarNFA()
+
+    b = NFAToDFA(a.nfa)
+    b.mostrarDFA()
+    b.minimizar()
+    b.mostrarMinDFA()
     
     nfa_data = ""
     nfa_data += f"ESTADOS: {{{', '.join(map(str, a.nfa.estados))}}}\n"
     nfa_data += f"SIMBOLOS: {{{', '.join(map(str, a.nfa.simbolo))}}}\n"
     nfa_data += f"ESTADO INICIAL: {{{a.nfa.estadoInicial}}}\n"
     nfa_data += f"ESTADOS DE ACEPTACION: {{{', '.join(map(str, a.nfa.estadosAceptados))}}}\n"
-    transiciones = [f"({desde_estado}, {simbolo}, {hacia_estado})" for desde_estado, simboloTransicion in a.nfa.transiciones.items() for simbolo, hacia_estado in simboloTransicion.items()]
+    transiciones = [f"({desde_estado}, {simbolo}, {hacia_estado})" for desde_estado, simboloTranscion in a.nfa.transiciones.items() for simbolo, hacia_estado in simboloTranscion.items()]
     nfa_data += f"TRANSICIONES: {{{', '.join(transiciones)}}}"
     write_to_file('nfa.txt', nfa_data)
+
+    dfa_data = ""
+    dfa_data += f"ESTADOS: {{{', '.join(map(str, b.dfa.estados))}}}\n"
+    dfa_data += f"SIMBOLOS: {{{', '.join(map(str, b.dfa.simbolo))}}}\n"
+    dfa_data += f"ESTADO INICIAL: {{{b.dfa.estadoInicial}}}\n"
+    dfa_data += f"ESTADOS DE ACEPTACION: {{{', '.join(map(str, b.dfa.estadosAceptados))}}}\n"
+    transiciones = [f"({desde_estado}, {simbolo}, {hacia_estado})" for desde_estado, simboloTranscion in b.dfa.transiciones.items() for simbolo, hacia_estado in simboloTranscion.items()]
+    dfa_data += f"TRANSICIONES: {{{', '.join(transiciones)}}}\n"
+    write_to_file('dfa.txt', dfa_data)
+    
+    min_dfa_data = ""
+    min_dfa_data += f"ESTADOS: {{{', '.join(map(str, b.minDFA.estados))}}}\n"
+    min_dfa_data += f"SIMBOLOS: {{{', '.join(map(str, b.minDFA.simbolo))}}}\n"
+    min_dfa_data += f"ESTADO INICIAL: {{{b.minDFA.estadoInicial}}}\n"
+    min_dfa_data += f"ESTADOS DE ACEPTACION: {{{', '.join(map(str, b.minDFA.estadosAceptados))}}}\n"
+    transiciones = [f"({desde_estado}, {simbolo}, {hacia_estado})" for desde_estado, simboloTranscion in b.minDFA.transiciones.items() for simbolo, hacia_estado in simboloTranscion.items()]
+    min_dfa_data += f"TRANSICIONES: {{{', '.join(transiciones)}}}\n"
+    write_to_file('min_dfa.txt', min_dfa_data)
+
+    while True:
+        try:
+            s = input('Ingrese una cadena para ver si es aceptada o no: ')
+            if b.Analysis(s):
+                print('Aceptada por DFA')
+            else:
+                print('No aceptada por DFA')
+            if b.AnalysisMinimizedDFA(s):
+                print('Aceptada por Minimized DFA')
+            else:
+                print('No aceptada por Minimized DFA')
+        except EOFError:
+            break
